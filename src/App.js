d3cae85741794fce22af99966e9ce2fc7159823c
Thiago <thiago.jp.221@gmail.com>
2023-02-26 21:12:53 -0300

    Adicionando pages, layouts e components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/home';
+import Company from './components/pages/company';
+import Contact from './components/contact';
+import NewProject from './components/pages/newproject';
+import Container from './components/layout/container';
+import NavBar from './components/layout/navbar';
+import Footer from './components/layout/footer';
+import Project from './components/pages/project';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar/>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/company' element={<Company/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/newproject' element={<NewProject/>}/>
+          <Route path='/project' element={<Project/>}/>
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
