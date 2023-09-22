69dee85f9592f6b87e752b4cf52e97a02513ee82
Thiago <thiago.jp.221@gmail.com>
2023-09-21 21:11:14 -0300

    adding build

--- src/components/service/serviceForm.js
+++ src/components/service/serviceForm.js
@@ -12,7 +12,7 @@ function ServiceForm({ handleSubmit, btnText, projectData }) {
     function submit(e) {
         e.preventDefault()
         projectData.services.push(service)
-        handleSubmit(projectData)
+        handleSubmit(projectData) 
     }
     
     function handleChange(e) {
